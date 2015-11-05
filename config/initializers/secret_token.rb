51f129d72cc18504e78625523cdee8625225f203
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-04 20:26:48 -0500

    renamed application to front_builder

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Vagrant::Application.config.secret_token = 'bc8df88f168cc487314f9b35478bc54d361ad9c7156bb33ea2a36050bdda7f79b87da393519456f46facc0d6f9159e68bb3e8968e189813f62143808f3ff5525'
+FrontBuilder::Application.config.secret_token = 'bc8df88f168cc487314f9b35478bc54d361ad9c7156bb33ea2a36050bdda7f79b87da393519456f46facc0d6f9159e68bb3e8968e189813f62143808f3ff5525'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Vagrant::Application.routes.draw do
+FrontBuilder::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
