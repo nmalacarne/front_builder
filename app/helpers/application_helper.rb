047526dd08c45a68a53e989dc18f71286414a50f
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-06 04:41:08 -0500

    added navigation links helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def nav_links
+    links = [] 
+
+    if user_signed_in?
+      links << {:text => 'Adventure Fronts', :target => :root}
+    else
+      links << {:text => 'Sign In', :target => :new_user_session}
+    end
+
+    links
+  end
 end
