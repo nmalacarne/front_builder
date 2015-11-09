398ef79c8493d78e8f67159d1cd788790b5f7412
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-09 14:48:03 -0500

    migrated 'adventure' resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151105063240) do
+ActiveRecord::Schema.define(:version => 20151109193919) do
+
+  create_table "adventures", :force => true do |t|
+    t.string   "title",       :default => "", :null => false
+    t.text     "description"
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.datetime "created_at",                             :null => false
