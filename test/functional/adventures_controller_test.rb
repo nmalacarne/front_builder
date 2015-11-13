78398b8650fc96aec24958b2570a4bd50a1b185b
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-13 15:07:30 -0500

    wrote some basic tests

--- test/functional/adventures_controller_test.rb
+++ test/functional/adventures_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class AdventuresControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
     @adventure = adventures(:one)
   end
--- test/unit/adventure_test.rb
+++ test/unit/adventure_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class AdventureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not allow blank title" do 
+    adventure = Adventure.new
+    assert !adventure.save, "Saved adventure without title"
+  end
 end
