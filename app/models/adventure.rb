0e7ca386a55d7fa8c7580a323eaeb880e493dde2
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-17 16:39:43 -0500

    fixed typo

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,6 +1,6 @@
 class Adventure < ActiveRecord::Base
   attr_accessible :description, :title
   validates_presence_of :title
-  has_many :dangers, dependant: :destroy
+  has_many :dangers, dependent: :destroy
   accepts_nested_attributes_for :dangers
 end
