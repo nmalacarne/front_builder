44a78b6cdf0548f8133f0a0897c94acf884c6bde
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-18 13:56:36 -0500

    reject dangers if blank

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -2,5 +2,5 @@ class Adventure < ActiveRecord::Base
   attr_accessible :description, :title, :dangers_attributes
   validates_presence_of :title
   has_many :dangers, dependent: :destroy
-  accepts_nested_attributes_for :dangers
+  accepts_nested_attributes_for :dangers, reject_if: :all_blank
 end
