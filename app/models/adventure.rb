ba2001b2a626938af0a797ba2414558fef9cd903
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-17 19:39:55 -0500

    adventures can now access dangers attributes

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,5 +1,5 @@
 class Adventure < ActiveRecord::Base
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :dangers_attributes
   validates_presence_of :title
   has_many :dangers, dependent: :destroy
   accepts_nested_attributes_for :dangers
