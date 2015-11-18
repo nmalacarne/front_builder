dec0b66e46c624afd86e1eee401ebff130dbe8b9
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-18 15:28:37 -0500

    allow adventures model to destory dangers

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -2,5 +2,5 @@ class Adventure < ActiveRecord::Base
   attr_accessible :description, :title, :dangers_attributes
   validates_presence_of :title
   has_many :dangers, dependent: :destroy
-  accepts_nested_attributes_for :dangers, reject_if: :all_blank
+  accepts_nested_attributes_for :dangers, reject_if: :all_blank, allow_destroy: true
 end
