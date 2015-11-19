848aa0bdec124252d5fd9a2657aab9e1a9ca7448
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-18 20:13:23 -0500

    'add adventure' link is now a button

--- app/helpers/adventures_helper.rb
+++ app/helpers/adventures_helper.rb
@@ -3,9 +3,13 @@ module AdventuresHelper
     new_object = form.object.send(association).klass.new
     id = new_object.object_id
     fields = form.fields_for(association, new_object, child_index: id) do |builder|
-      render(partial: partial_name, locals: {f: builder})
+      render partial: partial_name, locals: {f: builder} 
     end
 
-    link_to(copy, '#', class: 'add-fields', data: {id: id, fields: fields.gsub('\n', '')})
+    link_to '#', class: 'add-fields', data: {id: id, fields: fields.gsub('\n', '')}  do
+      button_tag type: 'button', class: 'button tiny' do
+        content_tag :span, copy
+      end
+    end
   end
 end
