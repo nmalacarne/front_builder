2676c6698830f9f93fbe81751595f49b52ec35f4
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-19 19:21:22 -0500

    dangers now have many grim portents; dangers now accept nested attributes of grim portents

--- app/models/danger.rb
+++ app/models/danger.rb
@@ -1,4 +1,7 @@
 class Danger < ActiveRecord::Base
   belongs_to :adventure
   attr_accessible :impending_doom, :impulse, :title
+  validates_presence_of :title
+  has_many :grim_portents, dependent: :destroy
+  accepts_nested_attributes_for :grim_portents, reject_if: :all_blank, allow_destroy: true
 end
