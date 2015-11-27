98fb1c7b4932ceaea90a51ea6418a70a0eec92e6
nmalacarne <nicholas.malacarne@gmail.com>
2015-11-27 17:11:34 -0500

    no longer install rails from apt; install bundler and use bundler to install correct rails deps; include nginx and postgres recipes; perform rake db:setup when finished installing gems

--- cookbooks/front-builder.com/recipes/default.rb
+++ cookbooks/front-builder.com/recipes/default.rb
@@ -5,5 +5,27 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 execute 'apt-get update'
 
-package 'rails'
+package 'libpq-dev'
 package 'nodejs'
+package 'bundler'
+
+bash 'navigate to project directory on ssh' do
+  code <<-EOH
+    echo 'cd /front-builder.com/' >> /home/vagrant/.bashrc
+  EOH
+end
+
+bash 'run bundler' do
+  code <<-EOH
+    cd /front-builder.com/ && bundle
+  EOH
+end
+
+include_recipe 'front-builder.com::nginx'
+include_recipe 'front-builder.com::postgres'
+
+bash 'setup database' do
+  code <<-EOH
+    cd /front-builder.com/ && rake db:setup
+  EOH
+end
